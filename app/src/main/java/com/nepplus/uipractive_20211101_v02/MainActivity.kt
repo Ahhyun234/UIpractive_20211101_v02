fb76fbd78abddfa3d14f5f96ebc5bde2cc3a4030
이아현 <suk03029@gmail.com>
2021-11-01 13:19:19 +0900

    Initial commit

---
+++ app/src/main/java/com/nepplus/uipractive_20211101_v02/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nepplus.uipractive_20211101_v02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
